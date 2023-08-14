708c4681a8f7d898c0d67ec5f1d1152dfa2adc2c
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-08-14 14:05:40 -0400

    merge conflict

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const view_routes = require('./view_routes'); 
 const express = require('express');
-// const api = require('/api');
-const postRoutes = require('/api/postRoutes');
-const commentRoutes = require('/api/commentRoutes;')
-const userRoutes = require('/api/userRoutes');
+// const api = require('./api');
+// const postRoutes = require('./api/postRoutes');
+// const commentRoutes = require('./api/commentRoutes;')
+// const userRoutes = require('./api/userRoutes');
 const router = express.Router();
 
 // router.use('/', api);
